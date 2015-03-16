a602c7d31bf4cc4bc5cd4f710ac88dcd76e02274
Monica Ung <monicaung3@gmail.com>
2015-03-15 20:48:46 -0400

    Added animation for Syphen's second ability tutorial. Can't test yet because powers are broken.

---
+++ Assets/Scripts/UI/TutorialVacuum.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TutorialVacuum : MonoBehaviour {
+
+	public List<GameObject> tutorials;
+	int tutorialIndex;
+
+	void Start () {
+		tutorialIndex = 0;
+		foreach (GameObject tutorial in tutorials) {
+			tutorial.SetActive(false);
+		}
+	}
+
+	public void NextTutorial() {
+		//Disable current tutorial panel
+		tutorials [tutorialIndex].SetActive (false);
+		tutorialIndex++;
+		if (tutorialIndex < tutorials.Count) {
+			//Enable next tutorial panel
+			tutorials [tutorialIndex].SetActive (true);
+		}
+	}
+}
