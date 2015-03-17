f6613ff087525cb5f51f23e854d36c0f2d7ca2f7
Shalon Liu <shalon.liu@gmail.com>
2015-03-17 00:49:12 -0400

    started adding resources to first level - not quite done yet

---
+++ Assets/Scripts/Environment/ShatterSelf.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShatterSelf : MonoBehaviour {
+	public float _velocityThreshold;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void OnCollisionEnter(Collision col) {
+		if (col.rigidbody) {
+			float velocity = col.rigidbody.velocity.sqrMagnitude;
+			if (velocity > _velocityThreshold)
+			{
+				PhysicsStatus ps = this.GetComponent<PhysicsStatus>();
+				if (ps && (ps.pullable || ps.pushable))
+				{
+					this.gameObject.GetComponent<Rigidbody>().isKinematic = false;
+				}
+			}
+		}
+	}
+	
+	//	public void switchToFractured(){
+	//		this.gameObject.SetActive(false);
+	//		_fractureSet.gameObject.SetActive(true);
+	//	}
+}
