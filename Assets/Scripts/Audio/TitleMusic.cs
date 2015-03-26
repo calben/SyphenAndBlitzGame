004bed5f072470473da63265262d452f9190700a
Marc Jarvis <jarvis.marc@gmail.com>
2015-03-26 05:53:04 -0400

    Music no longer dies on level load from start.
    	modified:   Assets/Audio/Music/Pre_Break_1.ogg
    	renamed:    Assets/Audio/Music/Pre_Break_1.ogg -> Assets/Audio/Music/Pre_Break_1_OLD3.ogg
    	new file:   Assets/Audio/Music/Pre_Break_1_OLD3.ogg.meta
    	modified:   Assets/Audio/Music/Pre_Break_2.ogg
    	renamed:    Assets/Audio/Music/Pre_Break_2.ogg -> Assets/Audio/Music/Pre_Break_2_OLD3.ogg
    	new file:   Assets/Audio/Music/Pre_Break_2_OLD3.ogg.meta
    	modified:   Assets/Scenes/StartScreen.unity
    	new file:   Assets/Scripts/Audio/TitleMusic.cs
    	new file:   Assets/Scripts/Audio/TitleMusic.cs.meta

---
+++ Assets/Scripts/Audio/TitleMusic.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleMusic : MonoBehaviour {
+	public AudioManager _audioManager;
+
+	// Use this for initialization
+	void Start () {
+		_audioManager = this.gameObject.GetComponent<AudioManager>();
+		DontDestroyOnLoad(gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnLevelWasLoaded(int level) {
+		if (level != 0)
+		{
+			_audioManager.Stop("MainMenu1", 1.0f);
+		}
+	}
+}
